7b21763528d8f0ac5bce8d47da7c5c418cedce4e
Tharrmeehan Krishnathasan <tharrmeehan@hotmail.com>
2024-02-02 09:51:48 +0100

    Added unshaded red sphere

--- RayTracing4Newbies/main.cpp
+++ RayTracing4Newbies/main.cpp
@@ -4,9 +4,25 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r)
 {
-    return color(0, 0, 0);
+
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -36,7 +52,8 @@ int main() {
     auto pixel_delta_v = viewport_u / image_height;
 
 	// Calculate location of upper left pixel
-
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -44,7 +61,11 @@ int main() {
 
     for (int j = 0; j < image_height; ++j) {
         for (int i = 0; i < image_width; ++i) {
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
